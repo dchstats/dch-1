fbae23b1d80a98a28cfc7182c470a92c8bd351d2
sushant <sushant4032@gmail.com>
2019-02-01 16:23:44 +0530

    dataset array model normalized. section and section data separated.

--- main.js
+++ main.js
@@ -4,24 +4,22 @@ function begin() {
 
 }
 
-
-
 var app = angular.module('dch', []);
 app.controller('ctrl', function ($scope) {
     /////////////////////////////////////////////////////////////////////////////// Constants
     $scope.shifts = ['other', 'First', 'Second', 'Night'];
 
     $scope.shovels = [
-        { id: 00, name: 'P&H-01', east: [false, 0, 0, 0, 0], west: [false, 0, 0, 0, 0] },
-        { id: 01, name: 'P&H-02', east: [false, 0, 0, 0, 0], west: [false, 0, 0, 0, 0] },
-        { id: 02, name: 'P&H-03', east: [false, 0, 0, 0, 0], west: [false, 0, 0, 0, 0] },
-        { id: 03, name: 'P&H-04', east: [false, 0, 0, 0, 0], west: [false, 0, 0, 0, 0] },
-        { id: 04, name: 'P&H-05', east: [false, 0, 0, 0, 0], west: [false, 0, 0, 0, 0] },
-        { id: 05, name: 'P&H-06', east: [false, 0, 0, 0, 0], west: [false, 0, 0, 0, 0] },
-        { id: 06, name: 'P&H-07', east: [false, 0, 0, 0, 0], west: [false, 0, 0, 0, 0] },
-        { id: 07, name: 'P&H-08', east: [false, 0, 0, 0, 0], west: [false, 0, 0, 0, 0] },
-        { id: 08, name: 'P&H-09', east: [false, 0, 0, 0, 0], west: [false, 0, 0, 0, 0] },
-        { id: 09, name: 'P&H-10', east: [false, 0, 0, 0, 0], west: [false, 0, 0, 0, 0] }
+        { id: '00', name: 'P&H-01', east: false, east_data: [0, 0, 0, 0], west: false, west_data: [0, 0, 0, 0] },
+        { id: '01', name: 'P&H-02', east: false, east_data: [0, 0, 0, 0], west: false, west_data: [0, 0, 0, 0] },
+        { id: '02', name: 'P&H-03', east: false, east_data: [0, 0, 0, 0], west: false, west_data: [0, 0, 0, 0] },
+        { id: '03', name: 'P&H-04', east: false, east_data: [0, 0, 0, 0], west: false, west_data: [0, 0, 0, 0] },
+        { id: '04', name: 'P&H-05', east: false, east_data: [0, 0, 0, 0], west: false, west_data: [0, 0, 0, 0] },
+        { id: '05', name: 'P&H-06', east: false, east_data: [0, 0, 0, 0], west: false, west_data: [0, 0, 0, 0] },
+        { id: '06', name: 'P&H-07', east: false, east_data: [0, 0, 0, 0], west: false, west_data: [0, 0, 0, 0] },
+        { id: '07', name: 'P&H-08', east: false, east_data: [0, 0, 0, 0], west: false, west_data: [0, 0, 0, 0] },
+        { id: '08', name: 'P&H-09', east: false, east_data: [0, 0, 0, 0], west: false, west_data: [0, 0, 0, 0] },
+        { id: '09', name: 'P&H-10', east: false, east_data: [0, 0, 0, 0], west: false, west_data: [0, 0, 0, 0] }
     ]
 
     /////////////////////////////////////////////////////////////////////////////// Properties    
@@ -35,7 +33,7 @@ app.controller('ctrl', function ($scope) {
 
     /////////////////////////////////////////////////////////////////////////////// Methods
     function formattedDate() {
-        var today = new Date;
+        var today = new Date();
         var dd = today.getDate();
         var hour = new Date().getHours();
         if (hour <= 14) dd = dd - 1;
@@ -58,26 +56,25 @@ app.controller('ctrl', function ($scope) {
     }
 
 
-
     $scope.refresh = function () {
-        $scope.eastShovelsTotal = [0, 0, 0, 0, 0];
-        $scope.westShovelsTotal = [0, 0, 0, 0, 0];
+        $scope.eastShovelsTotal = [0, 0, 0, 0];
+        $scope.westShovelsTotal = [0, 0, 0, 0];
 
         angular.forEach($scope.shovels, function (shovel) {
+            if (shovel.east) {
+                $scope.eastShovelsTotal[0] += shovel.east_data[0];
+                $scope.eastShovelsTotal[1] += shovel.east_data[1];
+                $scope.eastShovelsTotal[2] += shovel.east_data[2];
+                $scope.eastShovelsTotal[3] += shovel.east_data[3];
 
-            $scope.eastShovelsTotal[1] += shovel.east[1];
-            $scope.eastShovelsTotal[2] += shovel.east[2];
-            $scope.eastShovelsTotal[3] += shovel.east[3];
-            $scope.eastShovelsTotal[4] += shovel.east[4];
-            $scope.westShovelsTotal[1] += shovel.west[1];
-            $scope.westShovelsTotal[2] += shovel.west[2];
-            $scope.westShovelsTotal[3] += shovel.west[3];
-            $scope.westShovelsTotal[4] += shovel.west[4];
+            }
+            if (shovel.west) {
+                $scope.westShovelsTotal[0] += shovel.west_data[0];
+                $scope.westShovelsTotal[1] += shovel.west_data[1];
+                $scope.westShovelsTotal[2] += shovel.west_data[2];
+                $scope.westShovelsTotal[3] += shovel.west_data[3];
+            }
         });
-
-        //console.log($scope.eastShovelsTotal);
-        console.log($scope.westShovelsTotal);
-
 
     };
 });
